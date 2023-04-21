44e9a0885817ad6aa426347704cf68d727bfb0d5
Tabernero <2377512-davidtabernerom@users.noreply.gitlab.com>
2023-04-22 01:52:45 +0200

    Add the MoreColors class with all web colors available to use directly in Unity

---
+++ Runtime/Constants/MoreColors.cs
@@ -0,0 +1,179 @@
+using UnityEngine;
+
+
+
+
+namespace PossumScream.Constants
+{
+	public struct MoreColors
+	{
+		#region Web
+
+			public static readonly Color aliceblue = new Color(0.941176470588235f, 0.972549019607843f, 1f);
+			public static readonly Color antiquewhite = new Color(0.980392156862745f, 0.92156862745098f, 0.843137254901961f);
+			public static readonly Color aqua = new Color(0f, 1f, 1f);
+			public static readonly Color aquamarine = new Color(0.498039215686275f, 1f, 0.831372549019608f);
+			public static readonly Color azure = new Color(0.941176470588235f, 1f, 1f);
+			public static readonly Color beige = new Color(0.96078431372549f, 0.96078431372549f, 0.862745098039216f);
+			public static readonly Color bisque = new Color(1f, 0.894117647058824f, 0.768627450980392f);
+			public static readonly Color black = new Color(0f, 0f, 0f);
+			public static readonly Color blanchedalmond = new Color(1f, 0.92156862745098f, 0.803921568627451f);
+			public static readonly Color blue = new Color(0f, 0f, 1f);
+			public static readonly Color blueviolet = new Color(0.541176470588235f, 0.168627450980392f, 0.886274509803921f);
+			public static readonly Color brown = new Color(0.647058823529412f, 0.164705882352941f, 0.164705882352941f);
+			public static readonly Color burlywood = new Color(0.870588235294118f, 0.72156862745098f, 0.529411764705882f);
+			public static readonly Color cadetblue = new Color(0.372549019607843f, 0.619607843137255f, 0.627450980392157f);
+			public static readonly Color chartreuse = new Color(0.498039215686275f, 1f, 0f);
+			public static readonly Color chocolate = new Color(0.823529411764706f, 0.411764705882353f, 0.117647058823529f);
+			public static readonly Color coral = new Color(1f, 0.498039215686275f, 0.313725490196078f);
+			public static readonly Color cornflowerblue = new Color(0.392156862745098f, 0.584313725490196f, 0.929411764705882f);
+			public static readonly Color cornsilk = new Color(1f, 0.972549019607843f, 0.862745098039216f);
+			public static readonly Color crimson = new Color(0.862745098039216f, 0.0784313725490196f, 0.235294117647059f);
+			public static readonly Color cyan = new Color(0f, 1f, 1f);
+			public static readonly Color darkblue = new Color(0f, 0f, 0.545098039215686f);
+			public static readonly Color darkcyan = new Color(0f, 0.545098039215686f, 0.545098039215686f);
+			public static readonly Color darkgoldenrod = new Color(0.72156862745098f, 0.525490196078431f, 0.0431372549019608f);
+			public static readonly Color darkgray = new Color(0.662745098039216f, 0.662745098039216f, 0.662745098039216f);
+			public static readonly Color darkgreen = new Color(0f, 0.392156862745098f, 0f);
+			public static readonly Color darkgrey = new Color(0.662745098039216f, 0.662745098039216f, 0.662745098039216f);
+			public static readonly Color darkkhaki = new Color(0.741176470588235f, 0.717647058823529f, 0.419607843137255f);
+			public static readonly Color darkmagenta = new Color(0.545098039215686f, 0f, 0.545098039215686f);
+			public static readonly Color darkolivegreen = new Color(0.333333333333333f, 0.419607843137255f, 0.184313725490196f);
+			public static readonly Color darkorange = new Color(1f, 0.549019607843137f, 0f);
+			public static readonly Color darkorchid = new Color(0.6f, 0.196078431372549f, 0.8f);
+			public static readonly Color darkred = new Color(0.545098039215686f, 0f, 0f);
+			public static readonly Color darksalmon = new Color(0.913725490196078f, 0.588235294117647f, 0.47843137254902f);
+			public static readonly Color darkseagreen = new Color(0.56078431372549f, 0.737254901960784f, 0.56078431372549f);
+			public static readonly Color darkslateblue = new Color(0.282352941176471f, 0.23921568627451f, 0.545098039215686f);
+			public static readonly Color darkslategray = new Color(0.184313725490196f, 0.309803921568627f, 0.309803921568627f);
+			public static readonly Color darkslategrey = new Color(0.184313725490196f, 0.309803921568627f, 0.309803921568627f);
+			public static readonly Color darkturquoise = new Color(0f, 0.807843137254902f, 0.819607843137255f);
+			public static readonly Color darkviolet = new Color(0.580392156862745f, 0f, 0.827450980392157f);
+			public static readonly Color deeppink = new Color(1f, 0.0784313725490196f, 0.576470588235294f);
+			public static readonly Color deepskyblue = new Color(0f, 0.749019607843137f, 1f);
+			public static readonly Color dimgray = new Color(0.411764705882353f, 0.411764705882353f, 0.411764705882353f);
+			public static readonly Color dimgrey = new Color(0.411764705882353f, 0.411764705882353f, 0.411764705882353f);
+			public static readonly Color dodgerblue = new Color(0.117647058823529f, 0.564705882352941f, 1f);
+			public static readonly Color firebrick = new Color(0.698039215686274f, 0.133333333333333f, 0.133333333333333f);
+			public static readonly Color floralwhite = new Color(1f, 0.980392156862745f, 0.941176470588235f);
+			public static readonly Color forestgreen = new Color(0.133333333333333f, 0.545098039215686f, 0.133333333333333f);
+			public static readonly Color fuchsia = new Color(1f, 0f, 1f);
+			public static readonly Color gainsboro = new Color(0.862745098039216f, 0.862745098039216f, 0.862745098039216f);
+			public static readonly Color ghostwhite = new Color(0.972549019607843f, 0.972549019607843f, 1f);
+			public static readonly Color gold = new Color(1f, 0.843137254901961f, 0f);
+			public static readonly Color goldenrod = new Color(0.854901960784314f, 0.647058823529412f, 0.125490196078431f);
+			public static readonly Color gray = new Color(0.501960784313726f, 0.501960784313726f, 0.501960784313726f);
+			public static readonly Color green = new Color(0f, 0.501960784313726f, 0f);
+			public static readonly Color greenyellow = new Color(0.67843137254902f, 1f, 0.184313725490196f);
+			public static readonly Color grey = new Color(0.501960784313726f, 0.501960784313726f, 0.501960784313726f);
+			public static readonly Color honeydew = new Color(0.941176470588235f, 1f, 0.941176470588235f);
+			public static readonly Color hotpink = new Color(1f, 0.411764705882353f, 0.705882352941176f);
+			public static readonly Color indianred = new Color(0.803921568627451f, 0.36078431372549f, 0.36078431372549f);
+			public static readonly Color indigo = new Color(0.294117647058823f, 0f, 0.509803921568627f);
+			public static readonly Color ivory = new Color(1f, 1f, 0.941176470588235f);
+			public static readonly Color khaki = new Color(0.941176470588235f, 0.901960784313726f, 0.549019607843137f);
+			public static readonly Color lavender = new Color(0.901960784313726f, 0.901960784313726f, 0.980392156862745f);
+			public static readonly Color lavenderblush = new Color(1f, 0.941176470588235f, 0.96078431372549f);
+			public static readonly Color lawngreen = new Color(0.486274509803922f, 0.988235294117647f, 0f);
+			public static readonly Color lemonchiffon = new Color(1f, 0.980392156862745f, 0.803921568627451f);
+			public static readonly Color lightblue = new Color(0.67843137254902f, 0.847058823529412f, 0.901960784313726f);
+			public static readonly Color lightcoral = new Color(0.941176470588235f, 0.501960784313726f, 0.501960784313726f);
+			public static readonly Color lightcyan = new Color(0.87843137254902f, 1f, 1f);
+			public static readonly Color lightgoldenrodyellow = new Color(0.980392156862745f, 0.980392156862745f, 0.823529411764706f);
+			public static readonly Color lightgray = new Color(0.827450980392157f, 0.827450980392157f, 0.827450980392157f);
+			public static readonly Color lightgreen = new Color(0.564705882352941f, 0.933333333333333f, 0.564705882352941f);
+			public static readonly Color lightgrey = new Color(0.827450980392157f, 0.827450980392157f, 0.827450980392157f);
+			public static readonly Color lightpink = new Color(1f, 0.713725490196078f, 0.756862745098039f);
+			public static readonly Color lightsalmon = new Color(1f, 0.627450980392157f, 0.47843137254902f);
+			public static readonly Color lightseagreen = new Color(0.125490196078431f, 0.698039215686274f, 0.666666666666667f);
+			public static readonly Color lightskyblue = new Color(0.529411764705882f, 0.807843137254902f, 0.980392156862745f);
+			public static readonly Color lightslategray = new Color(0.466666666666667f, 0.533333333333333f, 0.6f);
+			public static readonly Color lightslategrey = new Color(0.466666666666667f, 0.533333333333333f, 0.6f);
+			public static readonly Color lightsteelblue = new Color(0.690196078431373f, 0.768627450980392f, 0.870588235294118f);
+			public static readonly Color lightyellow = new Color(1f, 1f, 0.87843137254902f);
+			public static readonly Color lime = new Color(0f, 1f, 0f);
+			public static readonly Color limegreen = new Color(0.196078431372549f, 0.803921568627451f, 0.196078431372549f);
+			public static readonly Color linen = new Color(0.980392156862745f, 0.941176470588235f, 0.901960784313726f);
+			public static readonly Color magenta = new Color(1f, 0f, 1f);
+			public static readonly Color maroon = new Color(0.501960784313726f, 0f, 0f);
+			public static readonly Color mediumaquamarine = new Color(0.4f, 0.803921568627451f, 0.666666666666667f);
+			public static readonly Color mediumblue = new Color(0f, 0f, 0.803921568627451f);
+			public static readonly Color mediumorchid = new Color(0.729411764705882f, 0.333333333333333f, 0.827450980392157f);
+			public static readonly Color mediumpurple = new Color(0.576470588235294f, 0.43921568627451f, 0.858823529411765f);
+			public static readonly Color mediumseagreen = new Color(0.235294117647059f, 0.701960784313725f, 0.443137254901961f);
+			public static readonly Color mediumslateblue = new Color(0.482352941176471f, 0.407843137254902f, 0.933333333333333f);
+			public static readonly Color mediumspringgreen = new Color(0f, 0.980392156862745f, 0.603921568627451f);
+			public static readonly Color mediumturquoise = new Color(0.282352941176471f, 0.819607843137255f, 0.8f);
+			public static readonly Color mediumvioletred = new Color(0.780392156862745f, 0.0823529411764706f, 0.52156862745098f);
+			public static readonly Color midnightblue = new Color(0.0980392156862745f, 0.0980392156862745f, 0.43921568627451f);
+			public static readonly Color mintcream = new Color(0.96078431372549f, 1f, 0.980392156862745f);
+			public static readonly Color mistyrose = new Color(1f, 0.894117647058824f, 0.882352941176471f);
+			public static readonly Color moccasin = new Color(1f, 0.894117647058824f, 0.709803921568627f);
+			public static readonly Color navajowhite = new Color(1f, 0.870588235294118f, 0.67843137254902f);
+			public static readonly Color navy = new Color(0f, 0f, 0.501960784313726f);
+			public static readonly Color oldlace = new Color(0.992156862745098f, 0.96078431372549f, 0.901960784313726f);
+			public static readonly Color olive = new Color(0.501960784313726f, 0.501960784313726f, 0f);
+			public static readonly Color olivedrab = new Color(0.419607843137255f, 0.556862745098039f, 0.137254901960784f);
+			public static readonly Color orange = new Color(1f, 0.647058823529412f, 0f);
+			public static readonly Color orangered = new Color(1f, 0.270588235294118f, 0f);
+			public static readonly Color orchid = new Color(0.854901960784314f, 0.43921568627451f, 0.83921568627451f);
+			public static readonly Color palegoldenrod = new Color(0.933333333333333f, 0.909803921568627f, 0.666666666666667f);
+			public static readonly Color palegreen = new Color(0.596078431372549f, 0.984313725490196f, 0.596078431372549f);
+			public static readonly Color paleturquoise = new Color(0.686274509803922f, 0.933333333333333f, 0.933333333333333f);
+			public static readonly Color palevioletred = new Color(0.858823529411765f, 0.43921568627451f, 0.576470588235294f);
+			public static readonly Color papayawhip = new Color(1f, 0.937254901960784f, 0.835294117647059f);
+			public static readonly Color peachpuff = new Color(1f, 0.854901960784314f, 0.725490196078431f);
+			public static readonly Color peru = new Color(0.803921568627451f, 0.52156862745098f, 0.247058823529412f);
+			public static readonly Color pink = new Color(1f, 0.752941176470588f, 0.796078431372549f);
+			public static readonly Color plum = new Color(0.866666666666667f, 0.627450980392157f, 0.866666666666667f);
+			public static readonly Color powderblue = new Color(0.690196078431373f, 0.87843137254902f, 0.901960784313726f);
+			public static readonly Color purple = new Color(0.501960784313726f, 0f, 0.501960784313726f);
+			public static readonly Color rebeccapurple = new Color(0.4f, 0.2f, 0.6f);
+			public static readonly Color red = new Color(1f, 0f, 0f);
+			public static readonly Color rosybrown = new Color(0.737254901960784f, 0.56078431372549f, 0.56078431372549f);
+			public static readonly Color royalblue = new Color(0.254901960784314f, 0.411764705882353f, 0.882352941176471f);
+			public static readonly Color saddlebrown = new Color(0.545098039215686f, 0.270588235294118f, 0.0745098039215686f);
+			public static readonly Color salmon = new Color(0.980392156862745f, 0.501960784313726f, 0.447058823529412f);
+			public static readonly Color sandybrown = new Color(0.956862745098039f, 0.643137254901961f, 0.376470588235294f);
+			public static readonly Color seagreen = new Color(0.180392156862745f, 0.545098039215686f, 0.341176470588235f);
+			public static readonly Color seashell = new Color(1f, 0.96078431372549f, 0.933333333333333f);
+			public static readonly Color sienna = new Color(0.627450980392157f, 0.32156862745098f, 0.176470588235294f);
+			public static readonly Color silver = new Color(0.752941176470588f, 0.752941176470588f, 0.752941176470588f);
+			public static readonly Color skyblue = new Color(0.529411764705882f, 0.807843137254902f, 0.92156862745098f);
+			public static readonly Color slateblue = new Color(0.415686274509804f, 0.352941176470588f, 0.803921568627451f);
+			public static readonly Color slategray = new Color(0.43921568627451f, 0.501960784313726f, 0.564705882352941f);
+			public static readonly Color slategrey = new Color(0.43921568627451f, 0.501960784313726f, 0.564705882352941f);
+			public static readonly Color snow = new Color(1f, 0.980392156862745f, 0.980392156862745f);
+			public static readonly Color springgreen = new Color(0f, 1f, 0.498039215686275f);
+			public static readonly Color steelblue = new Color(0.274509803921569f, 0.509803921568627f, 0.705882352941176f);
+			public static readonly Color tan = new Color(0.823529411764706f, 0.705882352941176f, 0.549019607843137f);
+			public static readonly Color teal = new Color(0f, 0.501960784313726f, 0.501960784313726f);
+			public static readonly Color thistle = new Color(0.847058823529412f, 0.749019607843137f, 0.847058823529412f);
+			public static readonly Color tomato = new Color(1f, 0.388235294117647f, 0.27843137254902f);
+			public static readonly Color turquoise = new Color(0.250980392156863f, 0.87843137254902f, 0.815686274509804f);
+			public static readonly Color violet = new Color(0.933333333333333f, 0.509803921568627f, 0.933333333333333f);
+			public static readonly Color wheat = new Color(0.96078431372549f, 0.870588235294118f, 0.701960784313725f);
+			public static readonly Color white = new Color(1f, 1f, 1f);
+			public static readonly Color whitesmoke = new Color(0.96078431372549f, 0.96078431372549f, 0.96078431372549f);
+			public static readonly Color yellow = new Color(1f, 1f, 0f);
+			public static readonly Color yellowgreen = new Color(0.603921568627451f, 0.803921568627451f, 0.196078431372549f);
+
+		#endregion
+	}
+}
+
+
+
+
+/*                                                                                            */
+/*            ____                                 _____                                      */
+/*           / __ \____  ____________  ______ ___ / ___/_____________  ____ _____ ___         */
+/*          / /_/ / __ \/ ___/ ___/ / / / __ `__ \\__ \/ ___/ ___/ _ \/ __ `/ __ `__ \        */
+/*         / ____/ /_/ (__  |__  ) /_/ / / / / / /__/ / /__/ /  /  __/ /_/ / / / / / /        */
+/*        /_/    \____/____/____/\__,_/_/ /_/ /_/____/\___/_/   \___/\__,_/_/ /_/ /_/         */
+/*                                                                                            */
+/*        Licensed under the Apache License, Version 2.0. See LICENSE.md for more info        */
+/*        David Tabernero M. @ PossumScream                      Copyright © 2021-2023        */
+/*        https://gitlab.com/possumscream                          All rights reserved        */
+/*                                                                                            */
+/*                                                                                            */
