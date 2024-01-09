478841b5dfef50f6a59c5d91674d2a14b4db4d24
Tabernero <16870364+davidtabernerom@users.noreply.github.com>
2024-01-09 16:58:48 +0100

    Add extension for LayerMask easy detection with layers

---
+++ Runtime/Extensions/LayerMaskExtensions.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+
+namespace PossumScream.Extensions
+{
+	public static class LayerMaskExtensions
+	{
+		public static bool Includes(this LayerMask mask, int layer)
+		{
+			return ((mask.value & 1 << layer) > 0);
+		}
+	}
+}
+
+
+/*                                                                                            */
+/*          ______                               _______                                      */
+/*          \  __ \____  ____________  ______ ___\  ___/_____________  ____  ____ ___         */
+/*          / /_/ / __ \/ ___/ ___/ / / / __ \__ \\__ \/ ___/ ___/ _ \/ __ \/ __ \__ \        */
+/*         / ____/ /_/ /__  /__  / /_/ / / / / / /__/ / /__/ /  / ___/ /_/ / / / / / /        */
+/*        /_/    \____/____/____/\____/_/ /_/ /_/____/\___/_/   \___/\__/_/_/ /_/ /__\        */
+/*                                                                                            */
+/*        Licensed under the Apache License, Version 2.0. See LICENSE.md for more info        */
+/*        David Tabernero M. @ PossumScream                      Copyright © 2021-2024        */
+/*        GitLab - GitHub: possumscream                            All rights reserved        */
+/*        - - - - - - - - - - - - -                                  - - - - - - - - -        */
+/*                                                                                            */
