36bf8e8466e17e0808a9f1dc7a2362457d6fb7fd
Tabernero <16870364+davidtabernerom@users.noreply.github.com>
2024-07-24 00:42:34 +0200

    Add Insert method to IDictionary extension to add or set

---
+++ Runtime/Extensions/IDictionaryExtensions.cs
@@ -0,0 +1,34 @@
+using System.Collections.Generic;
+
+
+namespace DragonResonance.Extensions
+{
+	public static class IDictionaryExtensions
+	{
+		public static void AddOrSet<TKey, TValue>(this IDictionary<TKey, TValue> dictionary, TKey key, TValue value)
+		{
+			if (!dictionary.ContainsKey(key))
+				dictionary.Add(key, value);
+			else
+				dictionary[key] = value;
+		}
+	}
+}
+
+
+/*       ________________________________________________________________       */
+/*           _________   _______ ________  _______  _______  ___    _           */
+/*           |        \ |______/ |______| |  _____ |       | |  \   |           */
+/*           |________/ |     \_ |      | |______| |_______| |   \__|           */
+/*           ______ _____ _____ _____ __   _ _____ __   _ _____ _____           */
+/*           |____/ |____ [___  |   | | \  | |___| | \  | |     |____           */
+/*           |    \ |____ ____] |___| |  \_| |   | |  \_| |____ |____           */
+/*       ________________________________________________________________       */
+/*                                                                              */
+/*           David Tabernero M.  <https://github.com/davidtabernerom>           */
+/*           Dragon Resonance    <https://github.com/dragonresonance>           */
+/*                  Copyright © 2021-2024. All rights reserved.                 */
+/*                Licensed under the Apache License, Version 2.0.               */
+/*                         See LICENSE.md for more info.                        */
+/*       ________________________________________________________________       */
+/*                                                                              */
