014f05ecce6798d54f1d1817b92689512d2e5107
Tabernero <16870364+davidtabernerom@users.noreply.github.com>
2024-08-17 12:47:32 +0200

    Create a WaitForFrames custom yield instruction

---
+++ Runtime/Extensions/WaitForFrames.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+
+namespace DragonResonance.Extensions
+{
+	public class WaitForFrames : CustomYieldInstruction
+	{
+		private readonly int _targetFrame;
+
+
+		public WaitForFrames(int frames)
+		{
+			_targetFrame = Time.frameCount + frames;
+		}
+
+
+		public override bool keepWaiting => (_targetFrame > Time.frameCount);
+	}
+}
+
+
+/*       ________________________________________________________________       */
+/*           _________   _______ ________  _______  _______  ___    _           */
+/*           |        \ |______/ |______| |  _____ |       | |  \   |           */
+/*           |________/ |     \_ |      | |______| |_______| |   \__|           */
+/*           ______ _____ _____ _____ __   _ _____ __   _ _____ _____           */
+/*           |____/ |____ [___  |   | | \  | |___| | \  | |     |____           */
+/*           |    \ |____ ____] |___| |  \_| |   | |  \_| |____ |____           */
+/*       ________________________________________________________________       */
+/*                                                                              */
+/*           David Tabernero M.  <https://github.com/davidtabernerom>           */
+/*           Dragon Resonance    <https://github.com/dragonresonance>           */
+/*                  Copyright © 2021-2024. All rights reserved.                 */
+/*                Licensed under the Apache License, Version 2.0.               */
+/*                         See LICENSE.md for more info.                        */
+/*       ________________________________________________________________       */
+/*                                                                              */
