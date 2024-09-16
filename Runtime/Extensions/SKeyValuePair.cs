994e5e9d4aa013086bf532a6e65d834a587b2be2
Tabernero <16870364+davidtabernerom@users.noreply.github.com>
2024-09-16 15:00:06 +0200

    Create a serializable KeyValuePair struct for editor

---
+++ Runtime/Extensions/SKeyValuePair.cs
@@ -0,0 +1,30 @@
+using System;
+
+
+namespace DragonResonance.Extensions
+{
+	[Serializable]
+	public struct SKeyValuePair<TKey, TValue>
+	{
+		public TKey key;
+		public TValue value;
+	}
+}
+
+
+/*       ________________________________________________________________       */
+/*           _________   _______ ________  _______  _______  ___    _           */
+/*           |        \ |______/ |______| |  _____ |       | |  \   |           */
+/*           |________/ |     \_ |      | |______| |_______| |   \__|           */
+/*           ______ _____ _____ _____ __   _ _____ __   _ _____ _____           */
+/*           |____/ |____ [___  |   | | \  | |___| | \  | |     |____           */
+/*           |    \ |____ ____] |___| |  \_| |   | |  \_| |____ |____           */
+/*       ________________________________________________________________       */
+/*                                                                              */
+/*           David Tabernero M.  <https://github.com/davidtabernerom>           */
+/*           Dragon Resonance    <https://github.com/dragonresonance>           */
+/*                  Copyright © 2021-2024. All rights reserved.                 */
+/*                Licensed under the Apache License, Version 2.0.               */
+/*                         See LICENSE.md for more info.                        */
+/*       ________________________________________________________________       */
+/*                                                                              */
