a9812a4671c0ae4bb04b8ec0636a950afeccd098
Tabernero <16870364+davidtabernerom@users.noreply.github.com>
2024-12-12 19:34:54 +0100

    Add a Tag attribute for best tag selection

---
+++ Editor/Attributes/TagAttributeDrawer.cs
@@ -0,0 +1,46 @@
+﻿#if UNITY_EDITOR
+
+
+using DragonResonance.Attributes;
+using UnityEditor;
+using UnityEngine;
+
+
+namespace DragonResonance.Editor.Attributes
+{
+	[CustomPropertyDrawer(typeof(TagAttribute))]
+	public class TagAttributeDrawer : PropertyDrawer
+	{
+		public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+		{
+			if (property.propertyType != SerializedPropertyType.String)
+				EditorGUI.HelpBox(position, "The Tag attribute works just on strings.", MessageType.Error);
+			else
+				property.stringValue = EditorGUI.TagField(position, label, property.stringValue);
+		}
+	}
+}
+
+
+#endif
+
+
+/*                                                                              */
+/*  |>  Based on the script of DMGregory @ Game Development Stack Exchange. <|  */
+/*                                                                              */
+/*       ________________________________________________________________       */
+/*           _________   _______ ________  _______  _______  ___    _           */
+/*           |        \ |______/ |______| |  _____ |       | |  \   |           */
+/*           |________/ |     \_ |      | |______| |_______| |   \__|           */
+/*           ______ _____ _____ _____ __   _ _____ __   _ _____ _____           */
+/*           |____/ |____ [___  |   | | \  | |___| | \  | |     |____           */
+/*           |    \ |____ ____] |___| |  \_| |   | |  \_| |____ |____           */
+/*       ________________________________________________________________       */
+/*                                                                              */
+/*           David Tabernero M.  <https://github.com/davidtabernerom>           */
+/*           Dragon Resonance    <https://github.com/dragonresonance>           */
+/*                  Copyright © 2021-2024. All rights reserved.                 */
+/*                Licensed under the Apache License, Version 2.0.               */
+/*                         See LICENSE.md for more info.                        */
+/*       ________________________________________________________________       */
+/*                                                                              */
