530c1891611b3a1446e73c4de8570d6c863dc4a6
Tabernero <16870364+davidtabernerom@users.noreply.github.com>
2025-08-29 03:27:21 +0200

    Create ComponentExtensions and FindAndGet extension

---
+++ Runtime/Extensions/ComponentExtensions.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+
+namespace DragonResonance.Extensions
+{
+	public static class ComponentExtensions
+	{
+		public static Component FindAndGet<T>(this Component field) where T : Component
+		{
+			if (field == null)
+				field = Object.FindAnyObjectByType<T>();
+			return field;
+		}
+	}
+}
+
+
+/*       ________________________________________________________________       */
+/*           _________   _______ ________  _______  _______  ___    _           */
+/*           |        \ |______/ |______| |  _____ |       | |  \   |           */
+/*           |________/ |     \_ |      | |______| |_______| |   \__|           */
+/*           ______ _____ _____ _____ __   _ _____ __   _ _____ _____           */
+/*           |____/ |____ [___  |   | | \  | |___| | \  | |     |____           */
+/*           |    \ |____ ____] |___| |  \_| |   | |  \_| |____ |____           */
+/*       ________________________________________________________________       */
+/*                                                                              */
+/*           David Tabernero M.  <https://github.com/davidtabernerom>           */
+/*           Dragon Resonance    <https://github.com/dragonresonance>           */
+/*                  Copyright © 2021-2025. All rights reserved.                 */
+/*                Licensed under the Apache License, Version 2.0.               */
+/*                         See LICENSE.md for more info.                        */
+/*       ________________________________________________________________       */
+/*                                                                              */
