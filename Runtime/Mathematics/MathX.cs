20ef405c1ddbd516800e3c669b4863ba286fc577
Tabernero <16870364+davidtabernerom@users.noreply.github.com>
2025-11-22 02:33:36 +0100

    Optimize MathX class, add cm to inches conversion factors

--- Runtime/Mathematics/MathX.cs
+++ Runtime/Mathematics/MathX.cs
@@ -1,70 +1,40 @@
 using UnityEngine;
 
 
-
-
 namespace DragonResonance.Mathematics
 {
 	public static class MathX
 	{
-		#region Publics - General
+		#region Operations
 
-
-			public static int TimesDivisible(int dividend, int divisor)
-			{
-				return (((dividend / divisor != 0) && (dividend % divisor == 0)) ? (TimesDivisible((dividend / divisor), divisor) + 1) : 0);
-			}
-
+			public static int TimesDivisible(int dividend, int divisor) => (((dividend / divisor != 0) && (dividend % divisor == 0)) ? (TimesDivisible((dividend / divisor), divisor) + 1) : 0);
 
 		#endregion
 
 
+		#region Performance
 
-
-		#region Publics - Performance
-
-
-			public static float Framerate(int frames, float time)
-			{
-				return (frames / time);
-			}
-
+			public static float Framerate(int frames, float time) => (frames / time);
 
 		#endregion
 
 
+		#region Audio
 
-
-		#region Publics - Unity-Specific
-
-
-			public static float LinearToMixerAudioVolume(float linearValue)
-			{
-				return (Mathf.Log10(Mathf.Clamp(linearValue, 0.0001f, 10f)) * 20f);
-			}
-
-
-			public static float MixerToLinearAudioVolume(float mixerValue)
-			{
-				return Mathf.Pow(10f, (Mathf.Clamp(mixerValue, -80f, 20f) / 20f));
-			}
-
-
-
-
-			public static float SceneLoadPercentage(AsyncOperation sceneLoadOperation)
-			{
-				return Mathf.Clamp01(sceneLoadOperation.progress / 0.9f);
-			}
-
+			public static float LinearToMixerAudioVolume(float linearValue) => (Mathf.Log10(Mathf.Clamp(linearValue, 0.0001f, 10f)) * 20f);
+			public static float MixerToLinearAudioVolume(float mixerValue) => Mathf.Pow(10f, (Mathf.Clamp(mixerValue, -80f, 20f) / 20f));
 
 		#endregion
 
 
+		#region Audio
+
+			public static float SceneLoadPercentage(AsyncOperation sceneLoadOperation) => Mathf.Clamp01(sceneLoadOperation.progress / 0.9f);
+
+		#endregion
 
 
 		#region Properties - Conversion
-
 
 			public const float Rad2Rev = 0.15915494f; // rev = rad / (2*pi)rad/rev
 			public const float Rev2Rad = 6.28318531f; // rad = rev * (2*pi)rad/rev
@@ -75,6 +45,8 @@ namespace DragonResonance.Mathematics
 			public const float RTT2Ping = 0.002f; // ping = RTT / 1000 * 2
 			public const float Ping2RTT = 500f; // RTT = ping * 1000 / 2
 
+			public const float Cm2Inches = 0.39370079f; // inches = cm / 2.54
+			public const float Inches2Cm = 2.54f; // cm = inches * 2.54
 			public const float Meters2Miles = 0.00062137f; // miles = 15625/25146000
 			public const float Miles2Meters = 1609.344f; // meters = 25146000/15625
 			public const float Kilometers2Miles = 0.62137119f; // miles = 15625/25146
@@ -85,12 +57,9 @@ namespace DragonResonance.Mathematics
 			public const float MetSec2Mph = 2.23693629f; // mph = m/s * 3600s/h / 1000m/km * 15625/25146
 			public const float Mph2MetSec = 0.44704f; // m/s = mph * 25146/15625 * 1000m/km / 3600s/h
 
-
 		#endregion
 	}
 }
-
-
 
 
 /*       ________________________________________________________________       */
